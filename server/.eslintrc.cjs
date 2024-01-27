46760189b49b2a7a6c244cba85353279b6ca6dbe
Andrej <amilanovi0@gmail.com>
2024-01-27 02:21:40 +0100

    Add configuration for TypeORM

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -27,4 +27,22 @@ module.exports = {
     // we know what we are doing
     'import/prefer-default-export': 'off',
   },
+
+  overrides: [
+    // TypeORM generates classes for migrations that do not use `this`.
+    {
+      files: ['src/database/migrations/**/*.ts'],
+      rules: {
+        'class-methods-use-this': 'off',
+      },
+    },
+
+    // for TypeORM relations
+    {
+      files: ['src/entities/**/*.ts'],
+      rules: {
+        'import/no-cycle': 'off',
+      },
+    },
+  ],
 };
