a44717c9b7411b44da60ffa854e682a6ee1c35d6
Andrej <amilanovi0@gmail.com>
2024-02-16 21:08:35 +0100

    Add routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,30 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { Database } from './database';
+import usersRoute from './routes/usersRoutes';
+import servicesRoute from './routes/servicesRoutes';
+import reviewsRoute from './routes/reviewsRoutes';
+import employeesRoutes from './routes/employeesRoutes';
+import appointmentsRoutes from './routes/appointmentsRoutes';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function createApp(db: Database) {
   const app = express();
 
   app.use(cors({ origin: 'http://localhost:5173' }));
   app.use(express.json());
 
-  app.use('/health', (_, res) => {
-    res.status(200).send('OK');
+  app.use('/health', (_req: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+  app.use('/users', usersRoute(db));
+  app.use('/services', servicesRoute(db));
+  app.use('/reviews', reviewsRoute(db));
+  app.use('/employees', employeesRoutes(db));
+  app.use('/appointments', appointmentsRoutes(db));
+
+  app.all('*', (_req: Request, res: Response) => {
+    res.sendStatus(404);
   });
 
   return app;
---
+++ server/tests/app.spec.ts
@@ -0,0 +1,21 @@
+import supertest from 'supertest';
+import { it, afterAll, describe } from 'vitest';
+import createApp from '../src/app';
+import { createTestDatabase } from './utils/createTestDatabase';
+
+const database = await createTestDatabase();
+const app = createApp(database);
+
+afterAll(() => {
+  database.destroy();
+});
+
+describe('route', () => {
+  it('/health should return 200', async () => {
+    await supertest(app).get('/health').expect(200);
+  });
+
+  it('should return 404 when not found', async () => {
+    await supertest(app).get('/not-found').expect(404);
+  });
+});
