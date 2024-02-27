96e49a695453aaa52da89b14717d2aa56f51c289
Andrej <amilanovi0@gmail.com>
2024-02-27 22:49:52 +0100

    Implement isAdmin middleware

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
 import config from '../config';
+import { Database } from '../database';
+import { User } from '../entities';
 
-export default function authenticateToken(
+export function authenticateToken(
   req: Request,
   res: Response,
   next: NextFunction
@@ -28,5 +30,24 @@ export default function authenticateToken(
         }
       }
     );
+  }
+}
+
+export async function isAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  db: Database
+) {
+  const { userId } = req;
+
+  const userRepo = db.getRepository(User);
+
+  const user = await userRepo.findOne({ where: { id: userId } });
+
+  if (user && user.isAdmin) {
+    next();
+  } else {
+    res.sendStatus(403);
   }
 }
