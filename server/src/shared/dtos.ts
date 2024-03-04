259bd96ea77d610b1d29e4633f40f7c2056e6f26
Andrej <amilanovi0@gmail.com>
2024-03-04 21:36:20 +0100

    Add shared folder to export dtos to client

---
+++ server/src/shared/dtos.ts
@@ -0,0 +1,15 @@
+import UserDto from '../modules/users/UserDto';
+import ReviewDto from '../modules/reviews/ReviewDto';
+import AppointmentDto from '../modules/appointments/AppointmentDto';
+import EmployeeDto from '../modules/employees/EmployeeDto';
+import SalonServiceDto from '../modules/salonServices/SalonServiceDto';
+import BusinessTimeDto from '../modules/businessTimes/BusinessTimeDto';
+
+export {
+  UserDto,
+  ReviewDto,
+  AppointmentDto,
+  EmployeeDto,
+  SalonServiceDto,
+  BusinessTimeDto,
+};
