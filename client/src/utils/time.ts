ae9d82b316333cfe2e9cca6d7110d16ea452f7f2
Andrej <amilanovi0@gmail.com>
2024-03-09 15:46:13 +0100

    Change month and day to be 2-digit

--- client/src/utils/time.ts
+++ client/src/utils/time.ts
@@ -2,8 +2,8 @@ export function formatDateGerman(dateString: string): string {
   const date: Date = new Date(dateString);
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
   };
