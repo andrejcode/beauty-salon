bdc5f6d7f603931bb952f1172f9a33b18bbf78a5
Andrej Milanović <amilanovi0@gmail.com>
2024-12-26 13:16:36 +0100

    Change toEqual to toBe in tests

--- server/src/utils/tests/auth.spec.ts
+++ server/src/utils/tests/auth.spec.ts
@@ -3,29 +3,29 @@ import { isStrongPassword, isValidEmail } from '../auth';
 
 describe('isValidEmail', () => {
   it('should return true when email is valid', () => {
-    expect(isValidEmail('email@gmail.com')).toEqual(true);
+    expect(isValidEmail('email@gmail.com')).toBe(true);
   });
 
   it('should return false when email is invalid', () => {
-    expect(isValidEmail('email@gmail')).toEqual(false);
-    expect(isValidEmail('emailgmail.com')).toEqual(false);
-    expect(isValidEmail('email@com')).toEqual(false);
+    expect(isValidEmail('email@gmail')).toBe(false);
+    expect(isValidEmail('emailgmail.com')).toBe(false);
+    expect(isValidEmail('email@com')).toBe(false);
   });
 });
 
 describe('isStrongPassword', () => {
   it('should return true when password is strong', () => {
-    expect(isStrongPassword('Password123')).toEqual(true);
-    expect(isStrongPassword('VeryStrongPassword#4931')).toEqual(true);
-    expect(isStrongPassword('PaSsWoRd321')).toEqual(true);
+    expect(isStrongPassword('Password123')).toBe(true);
+    expect(isStrongPassword('VeryStrongPassword#4931')).toBe(true);
+    expect(isStrongPassword('PaSsWoRd321')).toBe(true);
   });
 
   it('should return false when password is weak', () => {
-    expect(isStrongPassword('pass')).toEqual(false);
-    expect(isStrongPassword('password')).toEqual(false);
-    expect(isStrongPassword('PASSWORD')).toEqual(false);
-    expect(isStrongPassword('123456789')).toEqual(false);
-    expect(isStrongPassword('password123')).toEqual(false);
-    expect(isStrongPassword('PASSWORD123')).toEqual(false);
+    expect(isStrongPassword('pass')).toBe(false);
+    expect(isStrongPassword('password')).toBe(false);
+    expect(isStrongPassword('PASSWORD')).toBe(false);
+    expect(isStrongPassword('123456789')).toBe(false);
+    expect(isStrongPassword('password123')).toBe(false);
+    expect(isStrongPassword('PASSWORD123')).toBe(false);
   });
 });
